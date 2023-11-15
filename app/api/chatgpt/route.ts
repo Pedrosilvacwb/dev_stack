2f77b5fa4be55ba1f8d26f459aa7d66af6f8d01f
Pedro <pedro.silva@ringa.com.br>
2023-11-15 16:45:26 -0300

    Finish project

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -31,6 +31,6 @@ export const POST = async (request: Request) => {
 
     return NextResponse.json({ reply });
   } catch (error) {
-    return NextResponse.json({ error: error.message });
+    return NextResponse.json({ error });
   }
 };
