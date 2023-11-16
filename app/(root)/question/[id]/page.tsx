5f2916802d7318c6aa5a65785394d5cc7a7f9114
Pedro <pedro.silva@ringa.com.br>
2023-11-16 08:25:50 -0300

    Finish project

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -33,36 +33,36 @@ const Page = async ({ params, searchParams }: any) => {
       <div className="flex-start w-full flex-col">
         <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
           <Link
-            href={`/profile/${question.author.clerkId}`}
+            href={`/profile/${question?.author.clerkId}`}
             className="flex items-center justify-start gap-1"
           >
             <Image
-              src={question.author.picture}
+              src={question?.author.picture}
               className="rounded-full"
               width={22}
               height={22}
               alt="profile picture"
             />
             <p className="paragraph-semibold text-dark300_light700">
-              {question.author.name}
+              {question?.author.name}
             </p>
           </Link>
           <div className="flex justify-end">
             {" "}
             <Votes
               type="question"
-              itemId={JSON.stringify(question._id)}
-              userId={JSON.stringify(mongoUser._id)}
-              upvotes={question.upvotes.length}
-              hasupVoted={question.upvotes.includes(mongoUser._id)}
-              downvotes={question.downvotes.length}
-              hasdownVoted={question.downvotes.includes(mongoUser._id)}
-              hasSaved={mongoUser?.saved.includes(question._id)}
+              itemId={JSON.stringify(question?._id)}
+              userId={JSON.stringify(mongoUser?._id)}
+              upvotes={question?.upvotes.length}
+              hasupVoted={question?.upvotes.includes(mongoUser?._id)}
+              downvotes={question?.downvotes.length}
+              hasdownVoted={question?.downvotes.includes(mongoUser?._id)}
+              hasSaved={mongoUser?.saved.includes(question?._id)}
             />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
-          {question.title}
+          {question?.title}
         </h2>
       </div>
       <div className="mb-8 mt-5 flex flex-wrap gap-4">
@@ -71,28 +71,28 @@ const Page = async ({ params, searchParams }: any) => {
           className="small-medium text-dark400_light800"
           imgUrl="/assets/icons/clock.svg"
           title=" Asked"
-          value={` asked ${getTimestamp(question.createdAt)}`}
+          value={` asked ${getTimestamp(question?.createdAt)}`}
         />
         <Metric
           alt="messages"
           className="small-medium text-dark400_light800"
           imgUrl="/assets/icons/message.svg"
           title=" Answers"
-          value={formatAndDivideNumber(question.answers.length)}
+          value={formatAndDivideNumber(question?.answers.length)}
         />
         <Metric
           alt="Views"
           className="small-medium text-dark400_light800"
           imgUrl="/assets/icons/eye.svg"
           title=" Views"
-          value={formatAndDivideNumber(question.views)}
+          value={formatAndDivideNumber(question?.views)}
         />
       </div>
 
-      <ParseHTML data={question.content} />
+      <ParseHTML data={question?.content} />
 
       <div className="mt-8 flex flex-wrap gap-2">
-        {question.tags.map((tag: any) => (
+        {question?.tags.map((tag: any) => (
           <RenderTag
             key={tag._id}
             id={tag._id}
